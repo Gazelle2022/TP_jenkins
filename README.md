39255afe68ad556e6744fc8ba50457e4cfadcff4
Gazelle2022 <ghizlane.laghribi@gmail.com>
2025-12-17 15:58:15 +0100

    ajouter calculatrice

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # TP_jenkins
-premier test 
+test pipeline jenkins
---
+++ test.js
@@ -0,0 +1,22 @@
+const calc = require('./calculatrice');
+
+test('addition', () => {
+  expect(calc.addition(2, 3)).toBe(5);
+});
+
+test('soustraction', () => {
+  expect(calc.soustraction(5, 3)).toBe(2);
+});
+
+test('multiplication', () => {
+  expect(calc.multiplication(2, 4)).toBe(8);
+});
+
+test('division', () => {
+  expect(calc.division(6, 3)).toBe(2);
+});
+
+test('division par zéro', () => {
+  expect(() => calc.division(5, 0)).toThrow();
+});
+git 

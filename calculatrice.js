d8816292dd39422e84bb0dfb4b9d9b962e8335a9
Gazelle2022 <ghizlane.laghribi@gmail.com>
2025-12-17 16:38:57 +0100

    ajout fichier calculatrice

---
+++ calculatrice.js
@@ -0,0 +1,18 @@
+function addition(a, b) {
+  return a + b;
+}
+
+function soustraction(a, b) {
+  return a - b;
+}
+
+function multiplication(a, b) {
+  return a * b;
+}
+
+function division(a, b) {
+  if (b === 0) throw new Error("Division par zéro");
+  return a / b;
+}
+
+module.exports = { addition, soustraction, multiplication, division };
